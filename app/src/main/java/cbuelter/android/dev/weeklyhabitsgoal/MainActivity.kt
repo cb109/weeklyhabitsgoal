1b520938f4fa39728926ad43e6fab9de35e574b6
Christoph Buelter <buelter.christoph@gmail.com>
2019-11-13 23:00:34 +0100

    Add toolbar menu and a new empty activity
    We will use it to show UI to create/change/delete habits.

--- app/src/main/java/cbuelter/android/dev/weeklyhabitsgoal/MainActivity.kt
+++ app/src/main/java/cbuelter/android/dev/weeklyhabitsgoal/MainActivity.kt
@@ -1,8 +1,12 @@
 package cbuelter.android.dev.weeklyhabitsgoal
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TableLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -10,8 +14,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    val tableLayout by lazy { TableLayout(this) }
+
+    private val tableLayout by lazy { TableLayout(this) }
     private lateinit var habitViewModel: HabitViewModel
+    private val MANAGE_HABITS_ACTIVITY_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +37,21 @@ class MainActivity : AppCompatActivity() {
             val habitNames = habits.map { it.habit }
             createTable(this, tableLayout, habitNames)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+
+        if (id == R.id.manage_habits) {
+            val intent = Intent(this@MainActivity, ManageHabitsActivity::class.java)
+            startActivityForResult(intent, MANAGE_HABITS_ACTIVITY_REQUEST_CODE)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
